572b87f24a21746f98219d7073665b9be1fad6fa
gianmarcodiale <gianmarcodialessandro@gmail.com>
2022-03-30 16:58:40 +0200

    snack 7 correction

--- snack-7/main.js
+++ snack-7/main.js
@@ -3,8 +3,11 @@ SNACK 7
 Stampa le potenze di 2 fino a 1000.
 */
 
-for (n = 1; n <= 1000; n++) {
+for (n = 1; n < 100; n++) {
     //console.log(n);
     nPower2 = Math.pow(2, n);
-    console.log(nPower2);
-}
+    //console.log(nPower2);
+    if (nPower2 < 1000) {
+        console.log(nPower2);
+    } else {};
+};
