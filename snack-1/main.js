a9d63048626e48b1563391129a1717da906c4da4
gianmarcodiale <gianmarcodialessandro@gmail.com>
2022-03-30 10:19:51 +0200

    commit-snack-1

---
+++ snack-1/main.js
@@ -0,0 +1,24 @@
+/* 
+SNACK 1 
+L’utente inserisce due numeri in successione, con due prompt.
+Il software stampa il maggiore.
+*/
+
+// creo i prompt per inserire i numeri
+const firstNumber = prompt("Inserisci il primo numero:");
+const secondNumber = prompt("Inserisci il secondo numero:");
+
+// creo la condizione per verificare se uno è maggiore, minore o uguale all'altro
+if (firstNumber > secondNumber) {
+
+    console.log("Il primo numero inserito è maggiore del secondo!");
+
+} else if (secondNumber > firstNumber) {
+
+    console.log("Il secondo numero inserito è maggiore del primo!");
+
+} else {
+
+    console.log("I numeri inseriti sono uguali!");
+
+};
