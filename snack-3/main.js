afdfdf1894fec7c9499870e7e36d3900459eb4ad
gianmarcodiale <gianmarcodialessandro@gmail.com>
2022-03-30 11:45:32 +0200

    first commit

---
+++ snack-3/main.js
@@ -0,0 +1,13 @@
+/* 
+SNACK 3
+Il software deve chiedere per 10 volte all’utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti.
+*/
+
+for (let i = 0; i < 10; i++) {
+    let inputNumber = Number(prompt('Inserisci un numero'));
+    console.log(inputNumber);
+}
+
+
+
