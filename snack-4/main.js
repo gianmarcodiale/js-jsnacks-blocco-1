0f07884ddca8fc4c743c7bca97749e7f56191da0
gianmarcodiale <gianmarcodialessandro@gmail.com>
2022-03-30 12:25:31 +0200

    snack 4 commit

---
+++ snack-4/main.js
@@ -0,0 +1,18 @@
+/* 
+SNACK 4
+In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
+*/
+
+const guestList = ['Luca', 'Giovanni', 'Marco', 'Laura', 'Arianna', 'Giada'];
+
+let guestName = prompt('Qual è il tuo nome?');
+
+for (i = 0; i < guestList.length; i++) {
+    guestListIndex = guestList[i];
+    if (guestName == guestListIndex) {
+        console.log('Il tuo nome è nella lista, puoi entrare.');
+        break;
+    } else {
+        console.log('Mi dispiace non sei sulla lista, non puoi entrare.');
+    }
+};
